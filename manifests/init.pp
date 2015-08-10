d45fa1ea78ff01c8ec2f70a3b0b23c35b2df216f
Erlend Midttun <erlend.midttun@ntnu.no>
2015-08-10 09:06:59 +0200

    Added documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,34 +6,36 @@
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# [*staticfiles*]
+#   This is an array of the Puppet "file" type, with parameters.
 #
 # === Variables
 #
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# An array of "file"s, with parameters as valid for that type.
 #
 # === Examples
 #
 #  class { 'staticfiles':
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    'root' => { path  => '/root',
+#                owner => 'root',
+#                group => 'root',
+#                mode  => '0700',
+#                ensure => 'directory', },
+#    'root.dot.bashrc.ebm' => {
+#                path  => '/root/.bashrc.ebm',
+#                owner => 'root',
+#                group => 'root',
+#                mode  => '0644',
+#                source:  'puppet:///staticfiles/root/dot.bashrc.ebm'
 #  }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Erlend Midttun <erlend.midttun@ntnu.no>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Erlend Midttun <erlend.midttun@ntnu.no>
 #
 class staticfiles (
   $staticfiles     = {},
